ce6d864951615fe583ac7161f7ffa0e46e95ef7e
McConnell <ik.ugwuanyi@gmail.com>
2022-07-01 14:17:30 +0100

    added jobspider

---
+++ job_scraping/spiders/job_spider.py
@@ -0,0 +1,28 @@
+import scrapy
+
+
+class JobSpider(scrapy.Spider):
+    name = "jobspider"
+
+    start_urls = [
+        "https://pythonjobs.github.io/",
+    ]
+
+    def parse(self, response):
+        jobs = response.css("div.job")
+
+        for job in jobs:
+
+            job_info = job.css("span.info::text").get()
+            job_title = job.css("h1 a::text").get()
+            job_location = job_info[0]
+            date_added = job_info[1]
+            role_type = job_info[2]
+            company = job_info[3]
+            job_details = job.css("p.detail::text").get()
+
+            yield {
+                "title": job_title,
+                "role_type": role_type,
+                "details": job_details,
+            }
