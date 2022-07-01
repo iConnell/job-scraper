5aa88fc76bde47632d88c324c9e1ad961aa2b5f1
McConnell <ik.ugwuanyi@gmail.com>
2022-07-01 14:26:25 +0100

    updated jobspider

--- job_scraping/spiders/job_spider.py
+++ job_scraping/spiders/job_spider.py
@@ -13,7 +13,8 @@ class JobSpider(scrapy.Spider):
 
         for job in jobs:
 
-            job_info = job.css("span.info::text").get()
+            job_info = job.css("span.info::text").getall()
+
             job_title = job.css("h1 a::text").get()
             job_location = job_info[0]
             date_added = job_info[1]
@@ -23,6 +24,9 @@ class JobSpider(scrapy.Spider):
 
             yield {
                 "title": job_title,
+                "location": job_location,
+                "date_added": date_added,
+                "company": company,
                 "role_type": role_type,
                 "details": job_details,
             }
